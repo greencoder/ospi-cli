d8e4b6d5245c60d464da309d199113c32a109e29
Scott Newman <snewman18@gmail.com>
2013-07-15 13:07:04 -0600

    Changed the check PID utility to honor the maximum amount of minutes as configured in the opensprinkler.py file.

--- utilities/check_pids.py
+++ utilities/check_pids.py
@@ -4,6 +4,7 @@ import stat
 import time
 
 from opensprinkler import OpenSprinkler
+from opensprinkler import MAX_MINUTES_PER_STATION
 
 CUR_DIR = os.path.dirname(os.path.realpath(__file__))
 PARENT_DIR = os.path.abspath(os.path.join(CUR_DIR, os.pardir))
@@ -15,19 +16,20 @@ pid_files = [f for f in os.listdir(PARENT_DIR) if f.endswith('.pid')]
 needs_cleanup = False
 old_pid_files = []
 
-# Look through any pid files we found and see if they are older than 30 
-# minutes. If they are, we'll flag that we need to run some cleanup.
+# Look through any pid files we found and see if they are older than the 
+# max number of minutes that are configured. If any files older than the max age 
+# allowed are found, we'll flag them and run some cleanup.
 for file_name in pid_files:
     file_path = os.path.join(CUR_DIR, file_name)
     file_age_min = int(time.time() - os.stat(file_path)[stat.ST_MTIME])/60
-    if file_age_min > 35:
+    if file_age_min > (MAX_MINUTES_PER_STATION + 5):
         needs_cleanup = True
         old_pid_files.append(file_path)
 
 # If we found any old files, we need to delete the file and turn the system off
 # in case it's errantly running.
 if not needs_cleanup:
-    sys.exit()
+    sys.exit("No old PID files found. Aborting.")
 
 # If we got here, we should ensure that all stations are turned off.
 sprinkler = OpenSprinkler()
